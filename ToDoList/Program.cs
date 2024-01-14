6ca02d2a91fb4c3e3596e8f97fd050e0cf3ab5aa
Raen Reyes <raen.reyes11@outlook.com>
2024-01-13 22:18:23 -0800

    Add project files.

---
+++ ToDoList/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ToDoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepGoing = true;
+
+            List<string> toDoList = new List<string>();
+
+            ToDoList MiniAppliacation = new ToDoList(toDoList);
+
+            while (keepGoing)
+            {
+                Console.Write("Would you like to Add or Remove a List Item (A) or (R) enter 99 to end: ");
+                string userAddOrRemove = Console.ReadLine();
+
+                if (userAddOrRemove == "a" || userAddOrRemove == "A")
+                {
+                    MiniAppliacation.Add();
+                    for (int i = 0; i < toDoList.Count; i++)
+                    {
+
+                        Console.WriteLine(i + 1 + ": " + toDoList[i]);
+
+                    }
+
+                }
+
+                else if ((userAddOrRemove == "r" || userAddOrRemove == "R") && toDoList.Count > 0)
+                {
+                    MiniAppliacation.Remove();
+                    for (int i = 0; i < toDoList.Count; i++)
+                    {
+
+                        Console.WriteLine(i + 1 + ": " + toDoList[i]);
+                    }
+                }
+                else if (userAddOrRemove == "99")
+                {
+                    keepGoing = false;
+                }
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < toDoList.Count; i++)
+            {
+                Console.WriteLine(i + 1 + ": " + toDoList[i]);
+            }
+
+        }
+    }
+}
---
+++ ToDoList/ToDoList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class ToDoList
+    {
+        public List<string> ListOfThings { get; set; }
+
+        public ToDoList(List<string> list)
+        {
+            this.ListOfThings = list;
+        }
+        public List<string> Add()
+        {
+            Console.Write($"Add a Todo: ");
+            string userToDo = Console.ReadLine();
+            ListOfThings.Add(userToDo);
+
+            return ListOfThings;
+        }
+        public List<string> Remove()
+        {
+            Console.Write($"Remove which Todo is already completed: ");
+            string remove = Console.ReadLine();
+            int number = int.Parse(remove);
+            ListOfThings.RemoveAt(number - 1);
+
+            return ListOfThings;
+        }
+    }
+}
